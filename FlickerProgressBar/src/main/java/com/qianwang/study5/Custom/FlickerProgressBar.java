fc5e568a43282b4739071e49e5e5aff6c84dbac0
yicooll <459909255@qq.com>
2017-05-28 17:26:27 +0800

    commit

--- FlickerProgressBar/src/main/java/com/qianwang/study5/Custom/FlickerProgressBar.java
+++ FlickerProgressBar/src/main/java/com/qianwang/study5/Custom/FlickerProgressBar.java
@@ -3,13 +3,17 @@ package com.qianwang.study5.Custom;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.Shader;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.qianwang.study5.R;
@@ -18,7 +22,7 @@ import com.qianwang.study5.R;
  * Created by luo on 2017/5/27.
  */
 
-public class FlickerProgressBar extends View {
+public class FlickerProgressBar extends View implements Runnable {
 
     private int textSize;
     private int loadingColor;
@@ -26,15 +30,21 @@ public class FlickerProgressBar extends View {
     private int mWidth;
     private int mHeight;
     private int defHeight = 88;
-    private Bitmap bgBitmap;
+    private Bitmap pgBitmap;
     private Paint mPaint;
     private static final int MAX_PROGRESS = 100;
-    private int progress;
-    private boolean isStop = false;
+    private float progress;
+    private boolean isStop = true;
     private Bitmap flickerBitmap;//闪烁的图片
-    private int flickerLeft;  //闪烁图片的开始位置
+    private int flickerLeft;  //闪烁图片移动到最左的位置
     private String progressText;
     private Rect bound;
+    private Paint textPaint;
+    private boolean isFinish = false;
+    private int progressColor = loadingColor;
+    private Thread thread;
+    private BitmapShader bitmapShader;
+    private Canvas pgCanvas;
 
     public FlickerProgressBar(Context context) {
         this(context, null);
@@ -47,7 +57,7 @@ public class FlickerProgressBar extends View {
     public FlickerProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initAttrs(attrs);
-        bound=new Rect();
+        bound = new Rect();
 
     }
 
@@ -57,6 +67,20 @@ public class FlickerProgressBar extends View {
         loadingColor = array.getColor(R.styleable.FlickerProgressBar_loadingColor, Color.BLUE);
         stopColor = array.getColor(R.styleable.FlickerProgressBar_stopColor, Color.YELLOW);
     }
+
+
+    private void init() {
+        flickerBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.flicker);
+        flickerLeft = -flickerBitmap.getWidth();
+
+        pgBitmap = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        pgCanvas = new Canvas(pgBitmap);
+        thread = new Thread(this);
+        thread.start();
+
+
+    }
+
 
     private float dp2px(int dpValue) {
 
@@ -78,6 +102,10 @@ public class FlickerProgressBar extends View {
         }
         mWidth = siezWidth;
         setMeasuredDimension(mWidth, mHeight);
+
+        if (pgBitmap == null) {
+            init();
+        }
     }
 
     @Override
@@ -87,58 +115,75 @@ public class FlickerProgressBar extends View {
         drawBorder(canvas);
         //2.画进度
         drawProgress(canvas);
-        drawBitmap(bgBitmap);
         //3进度文字
         drawProgressText(canvas);
-
         //4画颜色渐变的进度文字
         drawColorProgressText(canvas);
 
 
     }
 
+    /**
+     * 歌词渐变也可以这样处理。或者用shader（渲染器）
+     *
+     * @param canvas
+     */
     private void drawColorProgressText(Canvas canvas) {
 
+        textPaint.setColor(Color.WHITE);
 
+        int xWidth = (getMeasuredWidth() - bound.width()) / 2;
+        int xHeight = (getMeasuredHeight() + bound.height()) / 2;
+        float progressWidth = (progress / MAX_PROGRESS) * getMeasuredWidth();
+        if (progressWidth > xWidth) {
+            canvas.save(Canvas.CLIP_SAVE_FLAG);
+            float right = Math.min(progressWidth, xWidth + bound.width());
+            canvas.clipRect(xWidth, 0, right, getMeasuredHeight());
+            canvas.drawText(progressText, xWidth, xHeight, textPaint);
+            canvas.restore();
+        }
     }
 
     private void drawProgressText(Canvas canvas) {
 
-        Paint textPaint=new Paint();
-        progressText=getProgressText();
-        textPaint.getTextBounds(progressText,0,progressText.length(),bound);
-        int xWidth=(getMeasuredWidth()-bound.width())/2;
-        int xHeight=(getMeasuredHeight()+bound.height())/2;
-        canvas.drawText(progressText,xWidth,xHeight,textPaint);
-
-    }
-
-    private void drawBitmap(Bitmap dbBitmap) {
-
+        textPaint = new Paint();
+        textPaint.setTextSize(textSize);
+        progressText = getProgressText();
+        textPaint.getTextBounds(progressText, 0, progressText.length(), bound);
+        int xWidth = (getMeasuredWidth() - bound.width()) / 2;    //进度文本开始的位置
+        int xHeight = (getMeasuredHeight() + bound.height()) / 2;  //进度文本结束的位置
+        canvas.drawText(progressText, xWidth, xHeight, textPaint);
 
     }
 
     private void drawProgress(Canvas canvas) {
 
-        mPaint.setColor(loadingColor);
-        mPaint.setStrokeWidth(0);
+        mPaint.setColor(progressColor);
+        mPaint.setStrokeWidth(6);
         mPaint.setStyle(Paint.Style.FILL);
-        int right = (progress / MAX_PROGRESS) * getMeasuredWidth();
-        bgBitmap = Bitmap.createBitmap(Math.max(right, 1), mHeight, Bitmap.Config.ARGB_8888);
-        canvas = new Canvas(bgBitmap);
-        canvas.drawColor(loadingColor);
+        float right = (progress / MAX_PROGRESS) * getMeasuredWidth();
+        pgCanvas.save(Canvas.CLIP_SAVE_FLAG);
+        pgCanvas.clipRect(0, 0, right, getMeasuredHeight());
+        pgCanvas.drawColor(progressColor);
+        pgCanvas.restore();
+
         if (!isStop) {
-            mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-            canvas.drawBitmap(flickerBitmap, flickerLeft, 0, mPaint);
+            mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+            pgCanvas.drawBitmap(flickerBitmap, flickerLeft, 0, mPaint);
             mPaint.setXfermode(null);
         }
+
+        //控制显示区域
+        bitmapShader = new BitmapShader(pgBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mPaint.setShader(bitmapShader);
+        canvas.drawRect(0, 0, mWidth, mHeight, mPaint);
     }
 
     private void drawBorder(Canvas canvas) {
 
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        mPaint.setColor(loadingColor);
+        mPaint.setColor(progressColor);
         mPaint.setStrokeWidth(6);
         mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawRect(0, 0, mWidth, mHeight, mPaint);
@@ -146,7 +191,109 @@ public class FlickerProgressBar extends View {
 
     public String getProgressText() {
 
+        if (!isFinish) {
+            if (!isStop) {
+                progressText = "下载中" + progress + "%";
+            } else {
+                if (progress == 0) {
+                    progressText = "下载";
+                } else {
+                    progressText = "继续";
+                }
+            }
+        } else {
+            progressText = "下载完成";
+        }
+        return progressText;
+    }
 
-        return null;
+    public void toggle() {
+
+        if (!isFinish) {
+
+            if (isStop) {
+                setStop(false);
+            } else {
+                setStop(true);
+            }
+        }
+    }
+
+    public void setProgress(float progress) {
+
+        if (!isStop) {
+            if (progress < MAX_PROGRESS) {
+                this.progress = progress;
+            } else {
+                this.progress = MAX_PROGRESS;
+                isFinish = true;
+            }
+        }
+
+    }
+
+    public float getProgress() {
+
+        return progress;
+    }
+
+    public boolean isStop() {
+
+        return isStop;
+    }
+
+    public boolean isFinish() {
+
+        return isFinish;
+    }
+
+    public void setStop(boolean stop) {
+        isStop = stop;
+        if (isStop) {
+            progressColor = stopColor;
+            thread.interrupt();
+        } else {
+            progressColor = loadingColor;
+            thread = new Thread(this);
+            thread.start();
+        }
+        invalidate();
+    }
+
+    public void reset() {
+
+        setStop(true);
+        progress = 0;
+        isFinish = false;
+        isStop = false;
+        progressColor = loadingColor;
+        progressText = "下载";
+        flickerLeft = -flickerBitmap.getWidth();
+        init();
+
+
+    }
+
+    public void finishLoad() {
+        isFinish = true;
+        setStop(true);
+    }
+
+    @Override
+    public void run() {
+        int width = flickerBitmap.getWidth();
+        try {
+            while (!isStop && !thread.isInterrupted()) {
+                flickerLeft += dp2px(5);
+                float progressWidth =(progress / MAX_PROGRESS) * getMeasuredWidth();
+                if (flickerLeft>= progressWidth) {
+                    flickerLeft = -width;
+                }
+                postInvalidate();
+                Thread.sleep(20);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- FlickerProgressBar/src/main/java/com/qianwang/study5/MainActivity.java
+++ FlickerProgressBar/src/main/java/com/qianwang/study5/MainActivity.java
@@ -1,17 +1,83 @@
 package com.qianwang.study5;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.qianwang.study5.Custom.FlickerProgressBar;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
+    private FlickerProgressBar flickerpb;
+    private Thread loadThread;
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1) {
+                int num = (int) msg.obj;
+                flickerpb.setProgress(num);
+                if (num == 100) {
+                    flickerpb.finishLoad();
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flickerpb = (FlickerProgressBar) findViewById(R.id.flickerpb);
 
+        flickerpb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flickerpb.toggle();
+                if (flickerpb.isStop()) {
+                    loadThread.interrupt();
+                } else {
+                    loading();
+                }
+            }
+        });
 
+    }
 
+    public void reLoad() {
+        loadThread.interrupt();
+        flickerpb.reset();
+        loading();
+    }
+
+    public void loading() {
+
+        loadThread = new Thread(this);
+        loadThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (!loadThread.isInterrupted()) {
+            try {
+                int progress = (int) flickerpb.getProgress();
+                Message msg = Message.obtain();
+                progress += 1;
+                msg.what = 1;
+                msg.obj = progress;
+                mHandler.sendMessage(msg);
+                if (progress == 100) {
+                    break;
+                }
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
